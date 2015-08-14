75f66cd99f6ff5d7ef3236ad89bbf4567899e361
Shou Takenaka <sasaminn@gmail.com>
2015-08-14 23:32:06 +0900

    Allow to upload image file

--- Presentation.WPF/MainWindow.xaml.cs
+++ Presentation.WPF/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Windows;
+﻿using ATRealize.Mobilution.ImageUploader.Presentation.WPF.ViewModels;
 using Microsoft.Win32;
-using System.Net.Http;
-using ATRealize.Mobilution.ImageUploader.Presentation.WPF.Exchange;
-using System.IO;
-using System.Runtime.Serialization.Json;
-using ATRealize.Mobilution.ImageUploader.Presentation.WPF.ViewModels;
-using ATRealize.Mobilution.ImageUploader.Presentation.WPF.MobilutionServiceReference;
-using System.Threading.Tasks;
+using System;
+using System.Windows;
 
 namespace ATRealize.Mobilution.ImageUploader.Presentation.WPF
 {
@@ -28,74 +22,15 @@ namespace ATRealize.Mobilution.ImageUploader.Presentation.WPF
             await ViewModel.SearchAsync();
         }
 
-        private void Upload(object sender, RoutedEventArgs e)
+        private async void Upload(object sender, RoutedEventArgs e)
         {
-            ViewModel.Images.Add(new Image { Id = 5, Title = "Foo" });
-            //var dialog = new OpenFileDialog();
-            //dialog.DefaultExt = "*.jpg";
-            //if (dialog.ShowDialog() == true)
-            //{
-            //    UploadSelectedFile(dialog.FileName);
-            //}
-        }
-
-        private async void UploadSelectedFile(string path)
-        {
-            using (var client = new HttpClient())
+            var dialog = new OpenFileDialog();
+            dialog.DefaultExt = "*.jpg";
+            if (dialog.ShowDialog() == true)
             {
-                var content = new MultipartFormDataContent();
-
-                var model = new ImageExchange { Title = Path.GetFileName(path) };
-                addImageToContent(content, model);
-                addFileToContent(content, path);
-                try
-                {
-                    var message = await client.PostAsync("http://localhost:51256/api/images", content);
-
-                    if (message.IsSuccessStatusCode)
-                    {
-                        MessageBox.Show("OK");
-                    }
-                    else
-                    {
-                        MessageBox.Show("NG");
-                    }
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show(e.ToString());
-                }
-            }
-        }
-
-        private void addImageToContent(MultipartFormDataContent content, ImageExchange model)
-        {
-            var part = new StringContent(serializeToJson(model));
-            part.Headers.Add("Content-Disposition", "form-data; name=\"json\"");
-            content.Add(part, "json");
-        }
-
-        private void addFileToContent(MultipartFormDataContent content, string path)
-        {
-            var fileName = Path.GetFileName(path);
-            var stream = File.OpenRead(path);
-            var part = new StreamContent(stream);
-            part.Headers.Add("Content-Type", "application/octet-stream");
-            part.Headers.Add("Content-Disposition", String.Format("form-data; name=\"file\"; filename=\"{0}\"", fileName));
-            content.Add(part, "file", fileName);
-        }
-
-        private string serializeToJson(ImageExchange model)
-        {
-            using (var stream = new MemoryStream())
-            {
-                var serializer = new DataContractJsonSerializer(typeof(ImageExchange));
-                serializer.WriteObject(stream, model);
-                stream.Position = 0;
-                using (var reader = new StreamReader(stream))
-                {
-                    return reader.ReadToEnd();
-                }
+                await ViewModel.CreateAsync(dialog.FileName);
+                await ViewModel.SearchAsync();
+                MessageBox.Show("OK");
             }
         }
     }
--- Presentation.WPF/ViewModels/MainViewModel.cs
+++ Presentation.WPF/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,19 @@ namespace ATRealize.Mobilution.ImageUploader.Presentation.WPF.ViewModels
             var client = new ImageServiceClient();
             var response = await client.SearchAsync(new SearchRequest(Criteria));
             response.SearchResult.ForEach(o => Images.Add(o));
+            return response;
+        }
+
+        public async Task<CreateResponse> CreateAsync(string path)
+        {
+            var request = new ImageRequest
+            {
+                Content = File.ReadAllBytes(path),
+                ContentType = "image/jpg",
+                FileName = Path.GetFileName(path)
+            };
+            var client = new ImageServiceClient();
+            var response = await client.CreateAsync(new CreateRequest(request));
             return response;
         }
     }
